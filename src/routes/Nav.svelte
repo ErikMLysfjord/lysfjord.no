ad092c1a57f5dec5fea7a012aca9b69f7356cf0e
Erik Menkin Lysfjord <erik.menkin@lysfjord.no>
2024-09-26 00:43:56 +0100

    feat: improve navbar links

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -5,19 +5,19 @@
 <nav class="navbar">
 	<div class:active={$page.url.pathname == '/'}>
 		<a class:active-link={$page.url.pathname == '/'} href="/"
-			>🏡<span class="removable-text">{' Home'}</span></a
+			><span class="removable-text">{'Home'}</span></a
 		>
 	</div>
 	<div class="links-right">
 		<div class:active={$page.url.pathname == '/books'}>
 			<a class:active-link={$page.url.pathname == '/books'} href="/books"
-				>📕<span class="removable-text">{' Books'}</span></a
+				><span class="removable-text">{'Books'}</span></a
 			>
 		</div>
 		<!-- <a href="/contact">Contact</a> -->
 		<div class:active={$page.url.pathname == '/resume'}>
 			<a class:active-link={$page.url.pathname == '/resume'} href="/resume"
-				>✏️<span class="removable-text">{' Resume'}</span></a
+				><span class="removable-text">{'Resume'}</span></a
 			>
 		</div>
 	</div>
@@ -38,19 +38,21 @@
 		opacity: 0.8;
 		color: #a9a9b3;
 		position: sticky;
+		font-size: 18px;
 	}
 
 	.navbar div {
 		padding: 0.25rem 0.5rem;
-		border-radius: 2.5px;
 	}
 
 	.active {
-		background: rgb(255, 204, 72, 0.3);
+		border-bottom: 2px solid #ffcc48;
+		padding-bottom: 3px;
 	}
 
 	.active-link {
 		color: white;
+		opacity: 0.8;
 	}
 
 	.links-right {
@@ -60,7 +62,8 @@
 
 	a:focus {
 		outline: 2px solid #ffcc48;
-		border-radius: 2px;
+		border-radius: 1px;
+		outline-offset: 6px;
 	}
 
 	@media (max-width: 600px) {
